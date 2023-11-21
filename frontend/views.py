76fc04d1ddf10fefd3f00e5ec3266256cd73b1f8
Siegheart-dev <58209283+Siegheart-dev@users.noreply.github.com>
2023-11-21 04:00:55 +0100

    clean1

--- frontend/views.py
+++ frontend/views.py
@@ -2,46 +2,37 @@ import requests
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
+def get_vehicle_info(vin_code):
+    url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinExtended/{vin_code}?format=json'
+    response = requests.get(url)
+    data = response.json()
+
+    variable_map = {
+        'Manufacturer Name': 'make',
+        'Model Year': 'model_year',
+        'Body Class': 'body_class',
+        'Gross Vehicle Weight Rating From': 'gross_weight',
+    }
+
+    vehicle_info = {variable_map.get(item.get('Variable'), ''): item.get('Value') for item in data.get('Results', [])}
+
+    return vehicle_info.get('make', ''), vehicle_info.get('model_year', ''), vehicle_info.get('body_class', ''), vehicle_info.get('gross_weight', '')
+
 def carfax(request):
-    
     vin_code = request.GET.get('vin-code', "")
-    make = ""
-    model_year = ""
-    body_class = ""
-    gross_weight = ""
-
-    if vin_code:
-        url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinExtended/{vin_code}?format=json'
-        response = requests.get(url)
-        data = response.json()
-
-        if 'Results' in data:
-            results = data['Results']
-            for item in results:
-                if item.get('Variable') == 'Manufacturer Name':
-                    make = item.get('Value')
-                elif item.get('Variable') == 'Model Year':
-                    model_year = item.get('Value')
-                elif item.get('Variable') == 'Body Class':
-                    body_class = item.get('Value')
-                elif item.get('Variable') == 'Gross Vehicle Weight Rating From':
-                    gross_weight = item.get('Value')
+    make, model_year, body_class, gross_weight = get_vehicle_info(vin_code)
 
     if make:
-        # Redirect to the results page with the 'make' data
+        # Redirect to the results page with the data
         return redirect(f'/carfax/results/?make={make}&model_year={model_year}&body_class={body_class}&gross_weight={gross_weight}')
     else:
         return render(request, 'frontend/carfax.html', {'error': 'Make not found for the given VIN code'})    
 
 def carfax_results(request):
-    make = request.GET.get('make', '')
-    model_year = request.GET.get('model_year', '')
-    body_class = request.GET.get('body_class', '')
-    gross_weight = request.GET.get('gross_weight', '')
+    vehicle_info = {key: request.GET.get(key, '') for key in ['make', 'model_year', 'body_class', 'gross_weight']}
 
-    # Check if body_class and gross_weight are 'None', replace with "No access to this information"
-    body_class = 'No access to this information' if body_class == 'None' else body_class
-    gross_weight = 'No access to this information' if gross_weight == 'None' else gross_weight
+    # Check if some of data are 'None', replace with "No access to this information"
+    for key in ['body_class', 'gross_weight']:
+        vehicle_info[key] = 'No access to this information' if vehicle_info[key] == 'None' else vehicle_info[key]
 
-    context = {'make': make, 'model_year': model_year, 'body_class': body_class, 'gross_weight': gross_weight}
-    return render(request, 'frontend/carfax_results.html', context)
+    return render(request, 'frontend/carfax_results.html', vehicle_info)
